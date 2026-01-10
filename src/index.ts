5d870239a92a81f02684ccd367862d87f125b48c
Carbon 碳苯 <carbon1024@foxmail.com>
2026-01-10 15:19:28 +0800

    refactor: simplify CLI structure by integrating main logic into cliMain function

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,5 @@
 #!/usr/bin/env node
-import { cac } from "cac";
-import * as p from "@clack/prompts";
-import { execa } from "execa";
-import { checkIsRepo } from "./utils/git.js";
-import { runDeepWikiAgent } from "./agent/provider.js";
 import "dotenv/config";
+import { cliMain } from "./cli/main.js";
 
-const cli = cac("litewiki");
-
-cli
-  .command("show", "🌟 analyze the current git repository")
-  .action(async () => {
-    // TODO: Actually, using git itself's command to check is enough
-    const isRepo = await checkIsRepo({ cwd: process.cwd() });
-    if (!isRepo) {
-      p.outro("Not a git repository");
-      return;
-    }
-
-    const { stdout } = await execa("git", ["log", "-1", "--pretty=%B"]);
-    p.outro(`Latest commit message: ${stdout}`);
-
-    const result = await runDeepWikiAgent(process.cwd());
-    p.outro(result);
-  });
-
-cli.help();
-cli.parse();
+await cliMain(process.argv);
